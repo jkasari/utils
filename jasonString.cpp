7b977fc4b93d4d0b2c0493526f38cda525952aad
Jason <jkasari@outlook.com>
2020-12-22 12:34:39 -0800

    add push_back signature

--- jasonString.cpp
+++ jasonString.cpp
@@ -1,27 +1,51 @@
 #include "jasonString.h"
 
+/**
+ * allocates a space on the heap for a |jasonString| to occupy
+ */
 jasonString::jasonString(const char* str) {
-    if (!str) {
-        return;
-    }
-    data = new char[strlen(str) + 1];
-    strcpy(data, str);
+  if (!str) {
+    return;
+  }
+  data = new char[strlen(str) + 1];
+  strcpy(data, str);
 }
 
+/**
+ * deletes the |jasonString| off of the heap
+ */
 jasonString::~jasonString() {
-    if (data) {
-      delete [] data;
-      data = nullptr;
-    }
+  if (data) {
+    delete[] data;
+    data = nullptr;
+  }
 }
 
+/**
+ * returns the length of a |jasonString|
+ */
 size_t jasonString::length() const {
-    if (!data) {
-        return 0;
-    }
-    return strlen(data);
+  if (!data) {
+    return 0;
+  }
+  return strlen(data);
 }
 
+/**
+ * returns the char array in a |jasonString| so that |nuttiest| can compare
+ * results
+ */
+const char* jasonString::inner() const { return data; }
+
+/**
+ * tells |ostream| how to display a |jasonString|
+ */
 std::ostream& operator<<(std::ostream& stream, const jasonString& jstr) {
-    return stream;
+  return stream;
 }
+
+/**
+ * finds a spot on the heap with enough room for the old |jasonString| plus one
+ * additional char
+ */
+void jasonString::push_back(const char c) {}
